5ed20855ae3b88e55d4cc6900df224b4a25cf281
Martin Karlsson <martin.karlsson@preciofishbone.se>
2021-04-12 11:04:25 +0200

    End of section 13

---
+++ client/src/app/_models/userParams.ts
@@ -0,0 +1,17 @@
+import { User } from 'src/app/_models/user';
+import { NgxGalleryAnimation } from '@kolkov/ngx-gallery';
+export class UserParams {
+    gender: string;
+    minAge = 18;
+    maxAge = 99;
+    pageNumber = 1;
+    pageSize = 5;
+    orderBy = 'lastActive'
+
+    
+    constructor(user: User) {
+        this.gender = user.gender === 'female' ? 'male' : 'female';
+        
+    }
+}
+
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,12 @@
+import { take } from 'rxjs/operators';
+import { AccountService } from './../../_services/account.service';
+import { UserParams } from './../../_models/userParams';
+import { Pagination } from './../../_models/pagination';
 import { Observable } from 'rxjs';
 import { MembersService } from './../../_services/members.service';
 import { Component, OnInit } from '@angular/core';
 import { Member } from 'src/app/_models/member';
+import { User } from 'src/app/_models/user';
 
 @Component({
   selector: 'app-member-list',
@@ -9,12 +14,37 @@ import { Member } from 'src/app/_models/member';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members$: Observable<Member[]>;
+  members: Member[];
+  pagination: Pagination;
+  userParams: UserParams;
+  user: User;
+  genderList= [{value: 'male', Display: 'Males'}, {value: 'female', Display: 'Females'}];
 
-  constructor(private membersService: MembersService) { }
+  constructor(private membersService: MembersService) {
+    this.userParams = this.membersService.getUserParams();
+  }
 
   ngOnInit(): void {
-    this.members$ = this.membersService.getMembers();
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.membersService.setUserParams(this.userParams);
+    this.membersService.getMembers(this.userParams).subscribe(response => {
+      this.members = response.result;
+      this.pagination = response.pagination;
+    })
+  }
+
+  resetFilters() {
+    this.userParams = this.membersService.resetUserParams();
+    this.loadMembers();
+  }
+
+  pageChanged(event: any) {
+    this.userParams.pageNumber = event.page;
+    this.membersService.setUserParams(this.userParams);
+    this.loadMembers();
   }
 
 }
